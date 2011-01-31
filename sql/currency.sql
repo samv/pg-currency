58a9c430bb89133ae6a5b3a2712d5b07b3eeb631
Sam Vilain <sam@vilain.net>
2011-02-01 12:11:21 +1300

    Add the #(currency) and currency->tla operators

--- sql/currency.sql
+++ sql/currency.sql
@@ -14,10 +14,12 @@ select '-100.006 eur'::currency as "-100.006 eur";
 -- formatting for display requires the currency_rates table
 select format('100 nzd'::currency) as "NZD 100.00";
 select format('-100.006 eur'::currency) as "€ -100.01";
+select #('123.456 gbp'::currency) as "£ 123.46";
 
 -- test conversion
 select change('100 nzd'::currency, 'btc') as "300.00 BTC";
 select format(change('100 nzd'::currency, 'gbp')) as "£ 42.86";
+select #('123.456 usd'::currency->'eur') as "€ 82.30";
 
 -- code and value
 select code('100nzd'::currency) as "NZD";
